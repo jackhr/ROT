689c9ecb55f39ca73da9a61ff158f7b59002a51c
jackhr <jc2o@mac.com>
2024-10-17 15:31:09 -0400

    Update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,8 @@
 <footer>
-
-    <section id="contact-banner"></section>
-
     <div class="inner">
-
-        <div id="copyright">© <?php echo date('Y'); ?> <?php echo $company_name; ?>   |   US$ to EC$ Exchange at 2.7   |   For discounted rate, enter 2 or more days.</div>
+        <div id="copyright">© <?php echo date('Y'); ?> <?php echo $company_name; ?></div>
     </div>
-
 </footer>
-
 </body>
 
 </html>
