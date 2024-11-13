32e68c90a0ab0a2ec207a803325c62ad4c6e855a
jackhr <jc2o@mac.com>
2024-11-12 23:24:41 -0500

    Update antigua section

--- index.php
+++ index.php
@@ -132,8 +132,9 @@ include_once 'includes/header.php';
 </section>
 <section id="antigua-section" class="still-carousel-section">
     <div>
-        <h3>Antigua</h3>
-        <p>The island awaits you. Rotten hill is just the beginning.</p>
+        <h3>Our World</h3>
+        <!-- <p>The island awaits you. Rotten hill is just the beginning.</p> -->
+        <p>Just a few reasons to get away to our retreat and explore our world.</p>
         <a class="chevron-link" href="/about/">
             <span>Explore</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
