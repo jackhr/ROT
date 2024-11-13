9581df6b55f866bd42fe8fc93e5fa956c94ccba7
jackhr <jc2o@mac.com>
2024-11-12 23:42:35 -0500

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,43 @@
 <footer>
+
+    <section id="contact-banner"></section>
+
     <div class="inner">
+        <div>
+            <div class="footer-nav">
+                <h6>Navigation</h6>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/cafe/">Cafe</a></li>
+                    <li><a href="/cottages/">Cottages</a></li>
+                    <li><a href="/events/">Events</a></li>
+                    <li><a href="/art-studio/">Art Studio</a></li>
+                    <li><a href="/meditation-center/">Meditation Center</a></li>
+                    <li><a href="/about/">About</a></li>
+                    <li><a href="/contact/">Contact</a></li>
+                </ul>
+            </div>
+            <div class="footer-logo">
+                <div>
+                    Rotten Hill
+                </div>
+            </div>
+            <div class="footer-contact">
+                <h6>Contact</h6>
+                <div class="contact-link">
+                    <span>Phone:</span>
+                    <a href="tel:+1 (268) 764-5594">+1 (268) 764-5594</a>
+                </div>
+                <div class="contact-link">
+                    <span>Email:</span>
+                    <a href="mailto:info@rottenhillstudios.com">info@rottenhillstudios.com</a>
+                </div>
+            </div>
+        </div>
+
         <div id="copyright">© <?php echo date('Y'); ?> <?php echo $company_name; ?></div>
     </div>
+
 </footer>
 </body>
 
