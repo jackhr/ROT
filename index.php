b1b007110038406b053022896beda6376a03d20a
jackhr <jc2o@mac.com>
2024-11-12 21:35:09 -0500

    Update intro section

--- index.php
+++ index.php
@@ -10,97 +10,105 @@ include_once 'includes/header.php';
 
 ?>
 
-<section id="intro-section">
+<section id="landing-header-section">
     <div class="inner">
         <video autoplay loop playsinline muted poster="/assets/images/video-posters/index-poster.jpg">
             <source src="assets/videos/index-video.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>
-        <div id="intro-section-title" style="display: none;">
+        <div id="landing-header-section-title" style="display: none;">
             <h1>Rotten Hill Studios</h1>
             <h2>Art Studio & Meditation Center</h2>
         </div>
         <div class="intro-joiner"></div>
     </div>
 </section>
-<section id="meditation-center-section" class="still-carousel-section">
+<section id="intro-section" class="still-carousel-section">
     <div class="inner">
-        <h2>Center Your Mind</h2>
         <div class="index-desc-wrapper">
-            <p>Our meditation center is the heart of the retreat, offering a sacred space to practice mindfulness and inner stillness. Surrounded by lush vegetation, the center features large windows that allow natural light to filter in, creating a calming atmosphere perfect for meditation, yoga, or quiet reflection.</p>
-            <p>Incorporating Buddhist design elements, such as prayer flags and statues, it evokes a sense of deep connection to the spiritual tradition. Here, guests can attend guided meditation sessions, participate in silent retreats, or simply take a moment for personal practice. Whether you’re new to meditation or an experienced practitioner, the space offers the perfect environment to explore your inner world and experience true peace of mind.</p>
+            <p>Welcome to Rotten Hill Studios, a space where creativity unfolds like a gentle breath, grounded and open. Set within the quiet pulse of the Caribbean, this sanctuary invites you to settle in, to listen, to be.</p>
+            <p>Our studios and cottages blend into the landscape, offering an understated harmony for the artist and the seeker alike. Here, creation is effortless—guided by the rhythms of nature and the simplicity of presence. Let your mind soften, your spirit unfurl, and allow inspiration to come as it will.</p>
+            <p>At Rotten Hill, there is nothing to strive for, only space to be.</p>
         </div>
-        <div class="section-divider"></div>
         <div class="carousel-container">
             <div class="carousel-header">
-                <h3>Meditation Center</h3>
-                <a class="chevron-link" href="/meditation-center/">
-                    <span>See more</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
-                        <path d="m9 18 6-6-6-6" />
-                    </svg>
-                </a>
+                <h3>A variety of experiences</h3>
             </div>
             <div id="meditation-center-carousel" class="carousel still right">
+                <!-- meditation center -->
                 <a class="carousel-cell" href="/meditation-center/">
-                    <span class="img-title">Meditation Center</span>
-                    <img src="assets/images/meditation-center/ROT-Day1-2409JMR25431.jpg" alt="Meditation Center">
-                    <div class="carousel-cell-overlay">
-                        <div class="chevron-link">
-                            <span>See more</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
-                                <path d="m9 18 6-6-6-6" />
-                            </svg>
+                    <div class="carousel-img-container">
+                        <img src="assets/images/meditation-center/ROT-Day1-2409JMR25650.jpg" alt="Meditation Center">
+                        <div class="carousel-cell-overlay">
+                            <div class="chevron-link">
+                                <span>See more</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
+                                    <path d="m9 18 6-6-6-6" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
+                    <span>Center Your Mind</span>
                 </a>
-                <a class="carousel-cell" href="/meditation-center/">
-                    <span class="img-title">Meditation Center</span>
-                    <img src="assets/images/meditation-center/ROT-Day1-2409JMR25525.jpg" alt="Meditation Center">
-                    <div class="carousel-cell-overlay">
-                        <div class="chevron-link">
-                            <span>See more</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
-                                <path d="m9 18 6-6-6-6" />
-                            </svg>
+                <!-- art studio -->
+                <a class="carousel-cell" href="/art-studio/">
+                    <div class="carousel-img-container">
+                        <img src="assets/images/events/ROT-Day1-Air4v2-2409JMR80922.jpg" alt="Aerial View">
+                        <div class="carousel-cell-overlay">
+                            <div class="chevron-link">
+                                <span>See more</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
+                                    <path d="m9 18 6-6-6-6" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
+                    <span>Creativity in Nature</span>
                 </a>
-                <a class="carousel-cell" href="/meditation-center/">
-                    <span class="img-title">Meditation Center</span>
-                    <img src="assets/images/meditation-center/ROT-Day1-2409JMR25545.jpg" alt="Meditation Center">
-                    <div class="carousel-cell-overlay">
-                        <div class="chevron-link">
-                            <span>See more</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
-                                <path d="m9 18 6-6-6-6" />
-                            </svg>
+                <!-- cottages -->
+                <a class="carousel-cell" href="/cottages/">
+                    <div class="carousel-img-container">
+                        <img src="assets/images/cottages/tamarind/ROT-Tamarind-BedArea-2410JMR26632.jpg" alt="Tamarind bed area" title="Tamarind bed area">
+                        <div class="carousel-cell-overlay">
+                            <div class="chevron-link">
+                                <span>See more</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
+                                    <path d="m9 18 6-6-6-6" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
+                    <span>Your Private Haven</span>
                 </a>
-                <a class="carousel-cell" href="/meditation-center/">
-                    <span class="img-title">Meditation Center</span>
-                    <img src="assets/images/meditation-center/ROT-Day1-2409JMR25650.jpg" alt="Meditation Center">
-                    <div class="carousel-cell-overlay">
-                        <div class="chevron-link">
-                            <span>See more</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
-                                <path d="m9 18 6-6-6-6" />
-                            </svg>
+                <!-- cafe -->
+                <a class="carousel-cell" href="/events/">
+                    <div class="carousel-img-container">
+                        <img src="assets/images/art-studio/ROT-Lifestyle-Textures-2410JMR49280.jpg" alt="Cafe" title="Cafe">
+                        <div class="carousel-cell-overlay">
+                            <div class="chevron-link">
+                                <span>See more</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
+                                    <path d="m9 18 6-6-6-6" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
+                    <span>Savor Serenity</span>
                 </a>
-                <a class="carousel-cell" href="/meditation-center/">
-                    <span class="img-title">Meditation Center</span>
-                    <img src="assets/images/meditation-center/ROT-Day1-2409JMR25692.jpg" alt="Meditation Center">
-                    <div class="carousel-cell-overlay">
-                        <div class="chevron-link">
-                            <span>See more</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
-                                <path d="m9 18 6-6-6-6" />
-                            </svg>
+                <!-- events -->
+                <a class="carousel-cell" href="/art-studio/">
+                    <div class="carousel-img-container">
+                        <img src="assets/images/events/ROT-Day1-Air4v2-2409JMR80937.jpg" alt="Aerial View">
+                        <div class="carousel-cell-overlay">
+                            <div class="chevron-link">
+                                <span>See more</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right">
+                                    <path d="m9 18 6-6-6-6" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
+                    <span>Gather and Celebrate</span>
                 </a>
             </div>
         </div>
